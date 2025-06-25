a28b00b51aa9f3b15d91e765753e6f33a42edced
Fábio <fabio.oliveira@poscontrole.com.br>
2025-06-25 16:21:43 -0300

    🚀 feat(sender): add Azure Service Bus client setup
    - read connection string, queue name, and worker count from environment variables
    - validate required environment variables
    - set default worker count if not provided
    - create Azure Service Bus client with the provided connection string
    - defer closing the client to release resources properly

--- cmd/sender/main.go
+++ cmd/sender/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"os"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
+)
+
+func main() {
+	connectionString := os.Getenv("SERVICEBUS_CONNECTION_STRING")
+	queueName := os.Getenv("SERVICEBUS_QUEUE_NAME")
+	workerCount := os.Getenv("WORKER_COUNT")
+
+	if connectionString == "" {
+		log.Fatal("A variável de ambiente SERVICEBUS_CONNECTION_STRING não foi definida.")
+	}
+	if queueName == "" {
+		log.Fatal("A variável de ambiente SERVICEBUS_QUEUE_NAME não foi definida.")
+	}
+	if workerCount == "" {
+		workerCount = "100"
+	}
+
+	client, err := azservicebus.NewClientFromConnectionString(connectionString, nil)
+	if err != nil {
+		log.Fatalf("Erro ao criar o client: %v", err)
+	}
+	defer client.Close(context.Background())
 }
